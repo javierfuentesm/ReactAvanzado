422cde3b8b12f1054e7c892603625aa3b8dc5cde
Javier <javier.fuentesm@hotmail.com>
2020-03-03 13:59:11 -0600

    First styled component

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import { Category } from './components/Category/index.jsx'
+export const App = () => <Category />
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-ReactDOM.render(<h1>Hola mundo</h1>, document.getElementById('app'))
+import { App } from './App.jsx'
+ReactDOM.render(<App />, document.getElementById('app'))
