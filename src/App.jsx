ff8977a1cf95627c0cc545516e2cc1418d96bc0c
Javier <javier.fuentesm@hotmail.com>
2020-03-05 00:46:41 -0600

    Se aprende  ausar SVG , generarlo , optimizarlo y convertirlo en un componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/index.jsx'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards/index.jsx'
+import { Logo } from './components/Logo/index.jsx'
 import { GlobalStyle } from './GlobalStyles'
-export const App = () => <div><GlobalStyle /><ListOfCategories /><ListOfPhotoCards /> </div>
+export const App = () => <div><GlobalStyle /> <Logo /> <ListOfCategories /><ListOfPhotoCards /> </div>
