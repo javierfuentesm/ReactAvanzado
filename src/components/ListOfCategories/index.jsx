57345867fbfd43ab32857594601b13f434ae2e82
Javier <javier.fuentesm@hotmail.com>
2020-03-09 23:53:07 -0600

    WIP

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category/index.jsx'
 import { List, Item } from './styles'
 import LoadingBar from 'react-top-loading-bar'
--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button, Img, ImgWrapper } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const ref = useRef(null)
+  useEffect(() => {
+    console.log(ref)
+  }, [ref])
   return (
-    <article>
+    <article ref={ref}>
       <a href={`/detail/${id}`}>
         <ImgWrapper>
           <Img src={src} />
