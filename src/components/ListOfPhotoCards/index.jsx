2e75ac9b1c70b20e65cc66e84e844de3581355b3
Javier <javier.fuentesm@hotmail.com>
2020-03-10 20:18:35 -0600

    Uso de local storage

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,10 +1,10 @@
-
 import React from 'react'
 import { PhotoCard } from '../PhotoCard/index.jsx'
 
 export const ListOfPhotoCards = () => (
   <ul>
-    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => <PhotoCard key={item} />)}
+    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
+      <PhotoCard key={item} id={item} />
+    ))}
   </ul>
-
 )
--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Button, Img, ImgWrapper, Article } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null)
   const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (error) {
+      return false
+    }
+  })
+
   useEffect(() => {
     Promise.resolve(
       typeof window.IntersectionObserver !== 'undefined'
@@ -22,6 +32,15 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       observer.observe(ref.current)
     })
   }, [ref])
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <Article ref={ref}>
       {show && (
@@ -31,8 +50,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button>
-            <MdFavoriteBorder size='32px' /> {likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size='32px' /> {likes} likes!
           </Button>
         </>
       )}
