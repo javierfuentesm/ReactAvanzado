341de95b78d0f8031f3944f4b83ef1844cad44a4
Javier <javier.fuentesm@hotmail.com>
2020-04-22 20:47:33 -0500

    Persiste localstorage y uso de useContext

--- src/pages/NotRegisteredUser.jsx
+++ src/pages/NotRegisteredUser.jsx
@@ -1,70 +1,69 @@
-import React from 'react'
-import Context from '../Context.jsx'
+import React, { useContext } from 'react'
+import { Context } from '../Context.jsx'
 import { UserForm } from '../components/UserForm/index.jsx'
 import { RegisterMutation } from '../containers/RegisterMutation.jsx'
 import { LoginMutation } from '../containers/LoginMutation.jsx'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {({ activateAuth }) => {
-      return (
-        <>
-          <RegisterMutation>
-            {(register, { data, loading, error }) => {
-              const onSubmit = async ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
 
-                try {
-                  await register({ variables })
-                  activateAuth()
-                  console.log(data)
-                } catch (error) {
-                  console.error(error)
-                }
-              }
-              const errorMsg =
+  return (
+    <>
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = async ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+
+            try {
+              const {
+                data: { signup }
+              } = await register({ variables })
+              activateAuth(signup)
+            } catch (error) {
+              console.error(error)
+            }
+          }
+          const errorMsg =
                 error && 'El usuario ya existe se encuentra registrado'
 
-              return (
-                <UserForm
-                  disabled={loading}
-                  error={errorMsg}
-                  onSubmit={onSubmit}
-                  title='Regístrate'
-                />
-              )
-            }}
-          </RegisterMutation>
-          <LoginMutation>
-            {(login, { data, loading, error }) => {
-              const onSubmit = async ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              onSubmit={onSubmit}
+              title='Regístrate'
+            />
+          )
+        }}
+      </RegisterMutation>
+      <LoginMutation>
+        {(login, { data, loading, error }) => {
+          const onSubmit = async ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
 
-                try {
-                  await login({ variables })
-                  activateAuth()
-                  console.log(data)
-                } catch (error) {
-                  console.error(error)
-                }
-              }
-              const errorMsg =
-                error && 'Los datos ingresados no son correctos'
+            try {
+              const {
+                data: { login: login2 }
+              } = await login({ variables })
+              activateAuth(login2)
+            } catch (error) {
+              console.error(error)
+            }
+          }
+          const errorMsg = error && 'Los datos ingresados no son correctos'
 
-              return (
-                <UserForm
-                  disabled={loading}
-                  error={errorMsg}
-                  onSubmit={onSubmit}
-                  title='Inicia Sesión'
-                />
-              )
-            }}
-          </LoginMutation>
-        </>
-      )
-    }}
-  </Context.Consumer>
-)
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              onSubmit={onSubmit}
+              title='Inicia Sesión'
+            />
+          )
+        }}
+      </LoginMutation>
+    </>
+  )
+}
