a6884c2370dd571aed4568333ccfafc2438f3bdf
Javier <javier.fuentesm@hotmail.com>
2020-04-25 23:46:12 -0500

    JWT y like photo

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -4,13 +4,13 @@ import { Link } from '@reach/router'
 import { FavButton } from '../FavButton/index.jsx'
 import { useNearScreen } from '../../hooks/useNearScreen.jsx'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation.jsx'
-import { useLocalStorage } from '../../hooks/useLocalStorage.jsx'
+// import { useLocalStorage } from '../../hooks/useLocalStorage.jsx'
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE, liked }) => {
-  const key = `like-${id}`
-  const [liked2, setLiked] = useLocalStorage(key, liked)
+/*   const [liked2, setLiked] = useLocalStorage(key, liked)
+  const key = `like-${id}` */
   const [show, ref] = useNearScreen()
 
   return (
@@ -25,13 +25,12 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE, liked }) => {
           <ToggleLikeMutation>
             {toggleLike => {
               const handleFavClick = () => {
-                !liked2 && toggleLike({ variables: { input: { id } } })
-                setLiked(!liked2)
+                toggleLike({ variables: { input: { id } } })
               }
 
               return (
                 <FavButton
-                  liked={liked2}
+                  liked={liked}
                   likes={likes}
                   onClick={handleFavClick}
                 />
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multi-spaces */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App.jsx'
@@ -5,7 +6,23 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import Context from './Context.jsx'
 const client = new ApolloClient({
-  uri: 'https://petgram-server.javierfuentesm.now.sh/graphql'
+  uri: 'https://petgram-server.javierfuentesm.now.sh/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    const { networkError } = error
+    if (networkError &&  networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 ReactDOM.render(
   <Context.Provider>
