7aff24a603829ee335a908b02815b87a92d8cda8
Javier <javier.fuentesm@hotmail.com>
2020-06-06 15:27:49 -0400

    Test end to end añadidos

--- cypress/integration/petgram/test_spec.js
+++ cypress/integration/petgram/test_spec.js
@@ -9,4 +9,16 @@ describe('Petgram', () => {
     cy.visit('/pet/2')
     cy.get('article')
   })
+  it('si podemos navegar con la nacbar a la home', () => {
+    cy.visit('/pet/3')
+    cy.get('nav a')
+      .first()
+      .click()
+    cy.url().should('include', '/')
+  })
+
+  it('los usuarios no registrados ven el formulario de registro e iniciio de sesion al ir a favs', () => {
+    cy.visit('/favs')
+    cy.get('form').should('have.length', 2)
+  })
 })
